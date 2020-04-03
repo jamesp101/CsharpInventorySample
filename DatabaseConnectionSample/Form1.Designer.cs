db7a2acea4e6b47c2828f29f0f0193292e006437
jamesp101 <jmspaul101@gmail.com>
2020-04-03 22:05:23 +0800

    First commit

---
+++ DatabaseConnectionSample/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace DatabaseConnectionSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listItem = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textQty = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textManufacturer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listItem
+            // 
+            this.listItem.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listItem.FullRowSelect = true;
+            this.listItem.Location = new System.Drawing.Point(52, 72);
+            this.listItem.Name = "listItem";
+            this.listItem.Size = new System.Drawing.Size(267, 355);
+            this.listItem.TabIndex = 0;
+            this.listItem.UseCompatibleStateImageBehavior = false;
+            this.listItem.View = System.Windows.Forms.View.Details;
+            this.listItem.SelectedIndexChanged += new System.EventHandler(this.listItem_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Price";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "QTY";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Manufacturer";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INVENTORY";
+            // 
+            // textId
+            // 
+            this.textId.Enabled = false;
+            this.textId.Location = new System.Drawing.Point(360, 104);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(77, 20);
+            this.textId.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(357, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(357, 146);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(248, 20);
+            this.textName.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(357, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Price";
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(357, 194);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(248, 20);
+            this.textPrice.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Qty";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textQty
+            // 
+            this.textQty.Location = new System.Drawing.Point(357, 246);
+            this.textQty.Name = "textQty";
+            this.textQty.Size = new System.Drawing.Size(248, 20);
+            this.textQty.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(357, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Manufacturer";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textManufacturer
+            // 
+            this.textManufacturer.Location = new System.Drawing.Point(357, 307);
+            this.textManufacturer.Name = "textManufacturer";
+            this.textManufacturer.Size = new System.Drawing.Size(248, 20);
+            this.textManufacturer.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(444, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 45);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(525, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 45);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(720, 475);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textManufacturer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textQty);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listItem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listItem;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textQty;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textManufacturer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DatabaseConnectionSample/Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DatabaseConnectionSample
+{
+    public partial class Form1 : Form
+    {
+        // My Connection String
+        string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\..\\..\\INventoryDatabase.accdb";
+
+        OleDbConnection con; // Code that handles the connection of the database
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // After the Form loads this will execute the connection of the database.
+        // If the connection is error the application will exit
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Try the code, if the code is error then it will run the catch block
+            try
+            {
+                // Setup the connection
+                con = new OleDbConnection(connectionString);
+
+                // try to connect to the database
+                // by opening the connection
+                con.Open();
+                //Show a message that the program has connected to the database
+                MessageBox.Show("Connected to the Database");
+
+                // close the connection
+                con.Close();
+
+                // Load the data from the Database into our listview
+                // See the void LoadListView() to see how to implement.
+                LoadListView();
+                LoadListView();
+
+            }
+            catch (Exception ex)
+            {
+                // If any error has occured in the try block statement 
+                // e.g Wrong Connection string
+                // it will run the catch codeblock
+
+
+                // Show this message if an error occured in thy try block code
+                MessageBox.Show("Cannot connect to the database");
+
+                // Show the Error
+                MessageBox.Show(ex.ToString());
+
+                // The app will close if there is an error to the connection of the database
+                Application.Exit();
+            }
+        }
+
+
+        // A custom function/method to be able to load 
+        // data into my ListView.
+        // I can call this block of code anywhere within 
+        // this class only.
+        void LoadListView()
+        {
+            // Open our connection first
+            con.Open();
+
+
+            // Setup SQL command
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = con; // Set the command connection to our connection we created.
+            cmd.CommandText = "SELECT * FROM tblItems"; //SQL command that load all data in the tblItems.
+
+            OleDbDataReader reader = cmd.ExecuteReader(); // Execute the SQL command and the returned data will be thrown to the reader object
+
+            // Clear the list so the data loaded wont repeat
+            listItem.Items.Clear();
+
+
+            while (reader.Read()) // Read every rows that returned in the SQL command
+            {
+
+                // ListViewItem represents a row in the  ListView
+                ListViewItem lvi = new ListViewItem(reader.GetValue(0).ToString()); // Set the first column of our list to the ID of the tblItems. 
+                lvi.SubItems.Add(reader.GetValue(1).ToString()); //Set the 2nd column of our list to the NAME of the tblItems. 
+                lvi.SubItems.Add(reader.GetValue(2).ToString()); //Set the 3rd column of our list to the PRICE of the tblItems. 
+                lvi.SubItems.Add(reader.GetValue(3).ToString());//Set the 4th column of our list to the QTY of the tblItems. 
+                lvi.SubItems.Add(reader.GetValue(4).ToString());//Set the 5th column of our list to the MANUFACTURER of the tblItems. 
+
+
+                listItem.Items.Add(lvi); // Add a row to the listView; 
+
+            }
+
+            // Close the connection of the database
+            con.Close();
+
+        }
+
+        // A custom function that adds item to the database
+        void AddItem()
+        {
+            // Open the connection 
+            con.Open();
+
+            //Setup SQL command
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO tblItems " +
+                              " (itemName, itemPrice, itemQty, itemManufacturer)  VALUES " +
+                              " (@name,    @price,    @qty,    @manufacturer)";
+            /*
+             * The @name will be replaced in thee cmd.Parameters.... by the textName.Text
+             */
+            cmd.Parameters.Add("@name", OleDbType.VarChar, 100).Value = textName.Text; // Replace the @name to the text of textName
+            cmd.Parameters.Add("@price", OleDbType.Double, 100).Value = textPrice.Text; // Replace the @price to the text of textName
+            cmd.Parameters.Add("@qty", OleDbType.Integer, 100).Value = textQty.Text; // Replace the @qty to the text of textQty
+            cmd.Parameters.Add("@manufacturer", OleDbType.VarChar, 100).Value = textManufacturer.Text; // Replace the @qty to the text of textManufacturer
+
+            /*
+             * Note the paramters for this is
+             * ("@string", OleDbType.DATATYPE, LIMIT)
+             * 
+             * If the column of your database is Text then Use OleDbType.VarChar
+             *  If the column of your database is a floating point then Use OleDbType.Double
+             *  If the column of your database is a Number then Use OleDbType.Integer
+             */
+
+
+            // Execute the command
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+
+            // CLose the connection
+            con.Close();
+
+            // Show a message that the item has been added
+            MessageBox.Show("Item added!", "",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        // Check if the textbox is correctly typed
+        // returns true if there is no error in the textboxes
+        bool Validate()
+        {
+            // https://stackoverflow.com/questions/9823836/checking-if-a-variable-is-of-data-type-double
+
+            double doubleResult;
+            int intResult;
+
+            bool isDouble = Double.TryParse(textPrice.Text, out doubleResult); // returns true if the textPrice is a double
+            bool isInteger = Int32.TryParse(textQty.Text, out intResult);// returnse true if the textQty is an integer
+
+            bool ret = true;
+
+            if (!isDouble) // if the textPrice is not a double then show a message box
+            {
+                ret = false;
+                MessageBox.Show("Invalid Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            if (!isInteger) // if the textQty is not an integer then show a messagebox
+            {
+                ret = false;
+                MessageBox.Show("Invalid Qty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            return ret; // if there are no errors then this function will return true
+
+
+
+
+        }
+
+
+        // Update Database implementation
+        void UpdateItem()
+        {
+
+            // Open the connection 
+            con.Open();
+
+            //Setup SQL command
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE tblItems " +
+                              " SET itemName=@name, itemPrice=@price, itemQty=@qty, itemManufacturer=@manufacturer " +
+                              " WHERE id=@id";
+            /*
+             * The @name will be replaced in thee cmd.Parameters.... by the textName.Text
+             */
+            cmd.Parameters.Add("@name", OleDbType.VarChar, 100).Value = textName.Text; // Replace the @name to the text of textName
+            cmd.Parameters.Add("@price", OleDbType.Double, 100).Value = textPrice.Text; // Replace the @price to the text of textName
+            cmd.Parameters.Add("@qty", OleDbType.Integer, 100).Value = textQty.Text; // Replace the @qty to the text of textQty
+            cmd.Parameters.Add("@manufacturer", OleDbType.VarChar, 100).Value = textManufacturer.Text; // Replace the @qty to the text of textManufacturer
+            cmd.Parameters.Add("@id", OleDbType.Integer, 100).Value = textId.Text; // Replace the @id to the text of id
+
+            /*
+             * Note the paramters for this is
+             * ("@string", OleDbType.DATATYPE, LIMIT)
+             * 
+             * If the column of your database is Text then Use OleDbType.VarChar
+             *  If the column of your database is a floating point then Use OleDbType.Double
+             *  If the column of your database is a Number then Use OleDbType.Integer
+             */
+
+
+            // Execute the command
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+
+            // CLose the connection
+            con.Close();
+
+            // Show a message that the item has been added
+            MessageBox.Show("Item Updated!");
+        }
+
+        // Delete Database implementation
+        void DeleteItem()
+        {
+
+            // Open the connection 
+            con.Open();
+
+            //Setup SQL command
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "DELETE FROM tblItems WHERE id=@id";
+
+            /*
+             * The @name will be replaced in thee cmd.Parameters.... by the textName.Text
+             */
+            cmd.Parameters.Add("@id", OleDbType.Integer, 100).Value = textId.Text; // Replace the @id to the text of id
+
+            /*
+             * Note the paramters for this is
+             * ("@string", OleDbType.DATATYPE, LIMIT)
+             * 
+             * If the column of your database is Text then Use OleDbType.VarChar
+             *  If the column of your database is a floating point then Use OleDbType.Double
+             *  If the column of your database is a Number then Use OleDbType.Integer
+             */
+
+
+            // Execute the command
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Item Deleted!","", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            // CLose the connection
+            con.Close();
+
+            // Show a message that the item has been added
+        }
+
+
+        // A custom function that clears all the textbox
+        // 
+        void ClearTextBoxes()
+        {
+            textId.Text = "";
+            textName.Text = "";
+            textPrice.Text = "";
+            textQty.Text = "";
+            textManufacturer.Text = "";
+        }
+
+        // Add button; Execute the AddItem function
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Executes the void AddItem();
+            // see void AddItem() to see the how to code
+
+            // If validate() returns a true(No error in the textboxes)
+            // Then Add the item to the Database
+            if (Validate())
+            {
+                AddItem();
+                
+                // Clear the textboxes after the item has been added
+                ClearTextBoxes();
+
+                // Refreshes the ListView
+                LoadListView();
+            }
+
+
+        }
+
+
+        // If the user selects an item in the ListView then set the selected item to the textboxes
+        private void listItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // If there are no selected Items
+            if (listItem.SelectedItems.Count == 0)
+            {
+                ClearTextBoxes();
+                button1.Enabled = true; // Enable the save button
+                button2.Enabled = false; // Disable the Edit Button
+                button3.Enabled = false; // Disable the Delete Button
+                return; // Stops the function
+            }
+            else
+            {
+                button1.Enabled = false; // Disable the save button
+                button2.Enabled = true; // Enable the Edit Button
+                button3.Enabled = true; // Enable the Delete Button
+            }
+
+            // Set the Selected item to the textbox
+            textId.Text = listItem.SelectedItems[0].SubItems[0].Text ; // [0] is the id in our listview
+            textName.Text = listItem.SelectedItems[0].SubItems[1].Text; // [1] is the name in our listview
+            textPrice.Text = listItem.SelectedItems[0].SubItems[2].Text; // [2] is the price in our listview
+            textQty.Text = listItem.SelectedItems[0].SubItems[3].Text; // [3] is the qty in our listview
+            textManufacturer.Text = listItem.SelectedItems[0].SubItems[4].Text; // [4] is the qty in our listview
+
+
+        }
+
+
+        // Update the Item
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Validate())
+            {
+                UpdateItem();
+                ClearTextBoxes();
+                // Refreshes the listview
+                LoadListView();
+
+                button1.Enabled = true; // Enable the save button
+                button2.Enabled = false; // Disable the Edit Button
+                button3.Enabled = false; // Disable the Delete Button
+            }
+        }
+
+        // Delete item
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // http://stackoverflow.com/questions/3036829/ddg#3036880
+            DialogResult result = MessageBox.Show("Delete this item?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                ClearTextBoxes();
+                LoadListView();
+
+                button1.Enabled = true; // Enable the save button
+                button2.Enabled = false; // Disable the Edit Button
+                button3.Enabled = false; // Disable the Delete Button
+            }
+
+        }
+    }
+}
